3e9c5ac9328fa5828b9632d9762611c4347d37f5
mojj5 <953172816@qq.com>
2020-06-18 09:35:38 +0800

    测试nocas

---
+++ test-untitled/consumer-untitled/src/main/java/com/consumer/test/ConsumerTestApp.java
@@ -0,0 +1,44 @@
+package com.consumer.test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConsumerTestApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerTestApp.class,args);
+    }
+
+    @LoadBalanced
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @RestController
+    public class TestController {
+
+        private final RestTemplate restTemplate;
+
+        @Autowired
+        public TestController(RestTemplate restTemplate) {this.restTemplate = restTemplate;}
+
+        @RequestMapping(value = "/echo", method = RequestMethod.GET)
+        public String echo() {
+            return restTemplate.getForObject("http://service-provider/", String.class);
+        }
+    }
+
+
+}
