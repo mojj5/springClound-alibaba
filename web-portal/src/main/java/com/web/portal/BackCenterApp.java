2c6e6fa0eafe316ece344af0adf1f54c3d4f5056
mojj5 <953172816@qq.com>
2020-06-23 09:59:24 +0800

    添加后台页面工程

---
+++ web-portal/src/main/java/com/web/portal/BackCenterApp.java
@@ -0,0 +1,13 @@
+package com.web.portal;
+
+import com.sun.glass.ui.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackCenterApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackCenterApp.class,args);
+    }
+}
