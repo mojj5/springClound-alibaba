652d54493834f8d4d5d72b2b76ed865b4cfd7985
mojj5 <953172816@qq.com>
2020-06-23 14:16:55 +0800

    打印fegin日志

---
+++ web-portal/src/main/java/com/web/portal/config/OpenFeignLogConfig.java
@@ -0,0 +1,14 @@
+package com.web.portal.config;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenFeignLogConfig {
+    @Bean
+    Logger.Level feignLoggerLeave(){
+        return Logger.Level.FULL;
+    }
+
+}
